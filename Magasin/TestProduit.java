408f477bfb2f592ad7d2152d84fc48cb8fd6139d
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2023-11-27 21:04:28 +0100

    Update TestProduit.java

--- Magasin/TestProduit.java
+++ Magasin/TestProduit.java
@@ -1,36 +1,114 @@
 import java.time.LocalDate;
+import java.util.Date;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class TestProduit {
     public static void main(String[] args) {
+        Date date = new Date("31/2/2024");
+        LocalDate Date = null;
+        produit product1 = new produit(1021, "Lait", "Delice", 0, Date);
+        produit product2 = new produit(2510, "Yaourt", "Vitalait", 0, Date);
+        produit product3 = new produit(3250, "Tomate", "Sicam", 1.200, Date);
+        product1.affiche();
+        product2.affiche();
+        product3.affiche();
+        product1.setPrix(0.700);
+        product1.affiche();
 
-        produit genericProduct = new produit();
-        LocalDate d3 = LocalDate.of(2023, 3, 30);
-        LocalDate d = LocalDate.of(2021, 10, 23);
-        LocalDate d2 = LocalDate.now();
-        produit p1 = new produit(1021, "lait", "delice", 0, d2);
-        produit p2 = new produit(2510, "yaourt", "vitalait", 0, d);
-        produit p3 = new produit(3250, "tomate", "sicam", 1.200, d);
-
-        System.out.println("Donner l'identifier de produit");
-        p1.affiche();
-        p2.affiche();
-        p3.affiche();
-        genericProduct.affiche();
-
-        p2.setPrix(700);
-        System.out.println("Le prix du produit modifié : " + p2.getPrix());
-        System.out.println(p1.toString());
-
-        p1.setDateExp(d);
-        p2.setDateExp(d3);
-        p1.affiche();
-        p2.affiche();
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the price of the product " + product2 + ":");
+        double prix = scan.nextDouble();
+        product2.setPrix(prix);
+        product2.affiche();
+        product1.toString();
+        product1.toString();
+        product1.toString();
         LocalDateTime t = LocalDateTime.now();
         System.out.println(t);
-        System.out.println("Comparaison avec produit p1: " + p1.comparer1(p2));
+        System.out.println("Enter the number of products in the Magasin: ");
+        int nombreProduits = 0;
+        do {
+            nombreProduits = scan.nextInt();
+        } while (nombreProduits > 50 || nombreProduits < 0);
+        produit pr = new produit();
+        produit[] TP = new produit[50];
+        for (int i = 0; i < nombreProduits; i++) {
+            System.out.println("Enter the id of the product: ");
+            pr.setIdentifier(scan.nextInt());
+            System.out.println("Enter the Libelle of the product: ");
+            pr.setLibelle(scan.next());
+            System.out.println("Enter the marque: ");
+            pr.setMarque(scan.next());
+            pr.setPrix(-0.1);
+            while (pr.getPrix() <= 0) {
+                System.out.println("Enter the price of the Product: ");
+                pr.setPrix(scan.nextDouble());
+            }
+            TP[i] = pr;
+        }
+        employe[] es = new employe[20];
+        Magasin mg = new Magasin(1, "Tunis", 50);
+        for (int i = 0; i < nombreProduits; i++) {
+            mg.ajouterProduit(TP[i]);
+        }
+        mg.afficherMagasin();
+        for (int i = 0; i < nombreProduits; i++) {
+            if (TP[i].comparer(product1) && (TP[i].comparer(product2)) && (TP[i].comparer(product3))) {
+                System.out.println("These are the same existing products");
+            } else {
+                System.out.println("The product does not exist twice");
+            }
+            if (TP[i].comparer(product1, product2)) {
+                System.out.println("These are the same existing products");
+            } else {
+                System.out.println("The product does not exist twice");
+            }
+        }
 
-        System.out.println("Comparaison avec produits p1 et p2: " + p1.comparer2(p1, p2));
+        employe[] er = new employe[20];
+        Magasin carrefour = new Magasin("Centre-Ville", 1, 1, TP, er, "mmg");
+        Magasin monoprix = new Magasin("Manzah 6", 2, 2, TP, es, "mgg");
+
+        caissier caissier1 = new caissier(1, "ala", "Adresse1", 160, 1);
+        caissier caissier12 = new caissier(1, "ala", "Adresse1", 160, 2);
+        vendeur vendeur1 = new vendeur(2, "ahmed", "Adresse2", 160, 0.8);
+        responsable responsable1 = new responsable(3, "amine", "Adresse3", 160, 200);
+        carrefour.ajouterEmploye(caissier1);
+        carrefour.ajouterEmploye(caissier12);
+        carrefour.ajouterEmploye(vendeur1);
+        carrefour.ajouterEmploye(responsable1);
+        caissier caissier2 = new caissier(1, "ala", "Adresse1", 160, 3);
+        vendeur vendeur2 = new vendeur(2, "ahmed", "Adresse2", 160, 0.8);
+        vendeur vendeur21 = new vendeur(2, "ahmed", "Adresse2", 160, 0.8);
+        vendeur vendeur22 = new vendeur(2, "ahmed", "Adresse2", 160, 0.8);
+        responsable responsable2 = new responsable(3, "amine", "Adresse3", 160, 200);
+        monoprix.ajouterEmploye(caissier2);
+        monoprix.ajouterEmploye(vendeur2);
+        monoprix.ajouterEmploye(vendeur21);
+        monoprix.ajouterEmploye(vendeur22);
+        monoprix.ajouterEmploye(responsable2);
+
+        carrefour.afficherMagasinEmployee();
+        monoprix.afficherMagasinEmployee();
+
+        System.out.println("Salaries of employees at carrefour");
+        System.out.println("Salary of caissier 1 at carrefour");
+        System.out.println(caissier1.calculSalaire());
+        System.out.println("Salary of vendeur 1 at carrefour");
+        System.out.println(((vendeur) vendeur1).calculsalaire()); 
+        System.out.println("Salary of responsable 1 at monoprix");
+        System.out.println(responsable1.calculSalaire());
+
+        System.out.println("Salaries of employees at monoprix");
+        System.out.println("Salary of caissier at monoprix");
+        System.out.println(caissier2.calculSalaire());
+        System.out.println("Salary of vendeur 1 at monoprix");
+        System.out.println(((vendeur) vendeur21).calculsalaire()); 
+        System.out.println("Salary of vendeur 2 at monoprix");
+        System.out.println(((vendeur) vendeur22).calculsalaire()); 
+        System.out.println("Salary of responsable at monoprix");
+        System.out.println(responsable2.calculSalaire());
+
     }
 }
