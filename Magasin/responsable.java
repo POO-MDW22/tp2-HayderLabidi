f94a681df63a78c621ca5eda37624dd73e52a556
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2023-11-27 21:23:16 +0100

    Delete Magasin directory

--- Magasin/responsable.java
+++
@@ -1,9 +0,0 @@
-public class responsable {
-    public int prime;
-    public responsable(){}
-    public responsable(int identifiant,String nom,String adresse,int nbrheure,int prime){
-        super( identifiant, nom, adresse, nbrheure);
-        this.prime=prime;
-    }
-
-}
